aa7348c0068bbc66f40d04a45fc65b770b59eda3
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-25 19:20:31 +0300

    Init utils

---
+++ src/main/java/com/example/nutritioncalculator/utils/DailyMenuConverter.java
@@ -0,0 +1,33 @@
+package com.example.nutritioncalculator.utils;
+
+import com.diplom.controller.dto.DailyMenuDto;
+import com.diplom.model.DailyMenu;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DailyMenuConverter {
+
+    public static DailyMenuDto convertDailyMenuEntityToDailyMenuDto(DailyMenu dailyMenu) {
+
+        return DailyMenuDto.builder()
+                .id(dailyMenu.getId())
+                .generalCalories(dailyMenu.getGeneralCalories())
+                .generalProteins(dailyMenu.getGeneralProteins())
+                .generalFats(dailyMenu.getGeneralFats())
+                .generalCarbonhydrates(dailyMenu.getGeneralCarbonhydrates())
+                .createdDate(dailyMenu.getCreatedDate())
+                .build();
+    }
+
+    public static DailyMenu convertDailyMenuDtoToDailyMenuEntity(DailyMenuDto dailyMenuDto) {
+
+        return DailyMenu.builder()
+                .id(dailyMenuDto.getId())
+                .generalCalories(dailyMenuDto.getGeneralCalories())
+                .generalProteins(dailyMenuDto.getGeneralProteins())
+                .generalFats(dailyMenuDto.getGeneralFats())
+                .generalCarbonhydrates(dailyMenuDto.getGeneralCarbonhydrates())
+                .createdDate(dailyMenuDto.getCreatedDate())
+                .build();
+    }
+}
