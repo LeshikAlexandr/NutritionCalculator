76115e555284a3b22239a728d0fb80a3d323f02c
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-25 17:47:05 +0300

    Added methods to get users from the database

--- src/main/java/com/example/nutritioncalculator/services/CustomerService.java
+++ src/main/java/com/example/nutritioncalculator/services/CustomerService.java
@@ -1,4 +1,40 @@
 package com.example.nutritioncalculator.services;
 
-public class CustomerService {
+import com.example.nutritioncalculator.exceptions.Exception;
+import com.example.nutritioncalculator.models.Customer;
+import com.example.nutritioncalculator.models.Role;
+import com.example.nutritioncalculator.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerService implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer findByLogin(String login) {
+        return customerRepository.findCustomerByLogin(login).orElseThrow(() -> new Exception("Не удалось найти пользователя"));
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Customer customer = findByLogin(login);
+        return new org.springframework.security.core.userdetails.User(customer.getLogin(), customer.getPassword(),
+                mapRolesToAuthorities(customer.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+    }
 }
