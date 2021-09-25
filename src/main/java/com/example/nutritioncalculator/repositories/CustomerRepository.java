f587fb30cc82719fc9c7ed0a4524d5aca30e622c
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-25 16:02:25 +0300

    Added Customer repository

---
+++ src/main/java/com/example/nutritioncalculator/repositories/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.example.nutritioncalculator.repositories;
+
+import com.example.nutritioncalculator.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    void deleteById(int id);
+
+    Optional<Customer> findById(int id);
+
+    Optional<Customer> findCustomerByLogin(String login);
+}
