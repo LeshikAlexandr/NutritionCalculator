d9f273679437aeb2a210d68ae878d7d7b9b2cfd4
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-27 20:18:32 +0300

    Initial commit

---
+++ src/main/java/com/example/nutritioncalculator/repositories/PhotoRepository.java
@@ -0,0 +1,17 @@
+package com.example.nutritioncalculator.repositories;
+
+import com.example.nutritioncalculator.models.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+
+    Optional<Photo> findByName(String name);
+
+    Optional<Photo> findByCustomer(int customerId);
+
+    Optional<Photo> findByCustomerLogin(String login);
+}
