8a3d52ab6737207ca859e63bc298638c0a36a2af
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-27 20:20:39 +0300

    Initial commit

---
+++ src/main/java/com/example/nutritioncalculator/services/PhotoService.java
@@ -0,0 +1,15 @@
+package com.example.nutritioncalculator.services;
+
+import com.example.nutritioncalculator.models.Photo;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface PhotoService {
+
+    void saveOrUpdate(MultipartFile file, String login);
+
+    Photo load(String fileName);
+
+    Photo getPhoto(int customerId);
+
+    Photo save(MultipartFile file);
+}
