64cb2bc3f6021691d87ddbec17c90a528d06f914
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-28 19:57:22 +0300

    Implemented methods save and get

--- src/main/java/com/example/nutritioncalculator/services/PostServiceImpl.java
+++ src/main/java/com/example/nutritioncalculator/services/PostServiceImpl.java
@@ -1,16 +1,24 @@
 package com.example.nutritioncalculator.services;
 
 import com.example.nutritioncalculator.controllers.dto.PostDto;
+import com.example.nutritioncalculator.exceptions.Exception;
 import com.example.nutritioncalculator.models.Post;
 import com.example.nutritioncalculator.repositories.CustomerRepository;
 import com.example.nutritioncalculator.repositories.PostRepository;
+import com.example.nutritioncalculator.utils.CustomerConverter;
+import com.example.nutritioncalculator.utils.PostConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -23,15 +31,29 @@ public class PostServiceImpl implements PostService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    // добавить фотосервис
+    @Autowired
+    private PhotoService photoService;
+
+    private final Path PhotoDirectoryPath = Paths.get("src/main/resources/static");
 
     @Override
     public void savePost(MultipartFile file, PostDto postDto, String login) {
+        if (file.getSize() != 0) {
+            postDto.setPhoto(photoService.save(file));
+        }
+        postDto.setCreatedDate(LocalDateTime.now());
+        postDto.setCustomerDto(CustomerConverter.convertCustomerEntityToCustomerDto(customerRepository.findCustomerByLogin(login)
+                .orElseThrow(() -> new Exception("Не удалось найти пользователя"))));
 
+        postRepository.save(PostConverter.convertPostDtoToPostEntity(postDto));
     }
 
     @Override
     public List<Post> getAllPosts() {
-        return null;
+        return postRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Post::getCreatedDate)
+                        .reversed())
+                .collect(Collectors.toList());
     }
 }
