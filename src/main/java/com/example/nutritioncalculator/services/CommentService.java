349713cad9753f675f773bdf693615573702182f
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-29 21:43:24 +0300

    Initial commit

---
+++ src/main/java/com/example/nutritioncalculator/services/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.nutritioncalculator.services;
+
+import com.example.nutritioncalculator.controllers.dto.CommentDto;
+import com.example.nutritioncalculator.models.Comment;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CommentService {
+
+    void saveComment(MultipartFile file, CommentDto commentDto, String login);
+
+    List<Comment> getAllComments();
+}
