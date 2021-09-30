b1630b78760aef18ce8b91da95fedf71dc64c5ba
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-09-30 19:36:53 +0300

    Initial commit

---
+++ src/main/java/com/example/nutritioncalculator/repositories/ProductDailyMenuRepository.java
@@ -0,0 +1,19 @@
+package com.example.nutritioncalculator.repositories;
+
+
+import com.example.nutritioncalculator.models.Eating;
+import com.example.nutritioncalculator.models.ProductDailyMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductDailyMenuRepository extends JpaRepository<ProductDailyMenu, Integer> {
+
+    List<ProductDailyMenu> findAllProductDailyMenuByDailyMenuId(int daily);
+
+    Optional<ProductDailyMenu> findByDailyMenuIdAndEatingAndProductId(int daily, Eating eating, int productId);
+
+}
