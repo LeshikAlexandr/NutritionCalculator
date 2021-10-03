c523b4a3e9d49d16eb23b1c45e5dbf9aea5be3c0
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-03 16:12:55 +0300

    Removed all unnecessary

--- src/main/java/com/example/nutritioncalculator/NutritionCalculatorApplication.java
+++ src/main/java/com/example/nutritioncalculator/NutritionCalculatorApplication.java
@@ -1,41 +1,12 @@
 package com.example.nutritioncalculator;
 
-import com.example.nutritioncalculator.models.Customer;
-import com.example.nutritioncalculator.models.Role;
-import com.example.nutritioncalculator.repositories.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @SpringBootApplication
-public class NutritionCalculatorApplication implements CommandLineRunner {
-
-    @Autowired
-    private CustomerRepository customerRepository;
+public class NutritionCalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NutritionCalculatorApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-//        Customer customer = new Customer();
-//        customer.setLogin("u");
-//        customer.setPassword("$2a$10$u3Wz.q.atVFmrdBWQYfu..oOvXywn7o2QHEsgFMy0em/wU1XJZA5i");
-//
-//        Role role = new Role();
-//        role.setName("USER");
-//
-//        Set<Role> roles = new HashSet<>();
-//        roles.add(role);
-//
-//        customer.setRoles(roles);
-//
-//        customerRepository.saveAndFlush(customer);
-
     }
 }
