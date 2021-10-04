a6c68e3d2b8685ad6797716900f330e6622b6a8e
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-04 23:29:47 +0300

    Updated validation

--- src/main/java/com/example/nutritioncalculator/controllers/dto/CustomerDto.java
+++ src/main/java/com/example/nutritioncalculator/controllers/dto/CustomerDto.java
@@ -21,13 +21,13 @@ public class CustomerDto {
 
     private int id;
     private String name;
-    @Min(value = 0, message = "Вес должен быть больше 0")
+    @Min(value = 1, message = "Вес должен быть 1 и больше")
     @Max(value=700, message = "Вес должен быть меньше 700")
     private int weight;
-    @Min(value = 0, message = "Рост должен быть больше 0")
+    @Min(value = 1, message = "Рост должен быть 1 и больше")
     @Max(value=280, message = "Рост должен быть меньше 280")
     private int height;
-    @Min(value = 0, message = "Возраст должен быть больше 0")
+    @Min(value = 1, message = "Возраст должен быть 1 и больше")
     @Max(value=99, message = "Возраст должен быть меньше 100")
     private int age;
     private Sex sex;
