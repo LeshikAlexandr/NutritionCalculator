30f337c7ed360f61bf078c1a36bf68dff6dc6c4c
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-04 22:06:27 +0300

    Added view controllers

--- src/main/java/com/example/nutritioncalculator/configs/MVCConfig.java
+++ src/main/java/com/example/nutritioncalculator/configs/MVCConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MVCConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("security/login");
+        registry.addViewController("/").setViewName("profile");
+        registry.addViewController("/customers/login");
+        registry.addViewController("/registration").setViewName("/security/registration");
     }
 }
