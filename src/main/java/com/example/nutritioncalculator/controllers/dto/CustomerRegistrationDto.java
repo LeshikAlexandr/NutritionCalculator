1636c18c0a73b2f471abeea0742e4e54839753dd
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-07 21:31:24 +0300

    Updated validation values

--- src/main/java/com/example/nutritioncalculator/controllers/dto/CustomerRegistrationDto.java
+++ src/main/java/com/example/nutritioncalculator/controllers/dto/CustomerRegistrationDto.java
@@ -16,11 +16,11 @@ public class CustomerRegistrationDto {
 
     private int id;
 
-    @Size(min = 5, max = 15, message = "Количество знаков должно быть в диапазоне от 5 до 15")
+    @Size(min = 4, max = 15, message = "Количество знаков должно быть в диапазоне от 4 до 15")
     @NotEmpty(message = "Поле должно быть заполнено")
     private String login;
 
-    @Size(min = 5, message = "Минимальное количество знаков - 5")
+    @Size(min = 4, message = "Минимальное количество знаков - 4")
     @NotEmpty(message = "Поле должно быть заполнено")
     private String password;
 }
