2d66743a4c306df213fb8f9464ea025a69cc9683
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-09 20:24:15 +0300

    Initial commit foodDiary and some update foodDiary.jsp

---
+++ src/main/java/com/example/nutritioncalculator/controllers/FoodDiaryController.java
@@ -0,0 +1,31 @@
+package com.example.nutritioncalculator.controllers;
+
+import com.example.nutritioncalculator.controllers.dto.CustomerDto;
+import com.example.nutritioncalculator.services.CustomerService;
+import com.example.nutritioncalculator.services.DailyMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/food-diaries")
+public class FoodDiaryController {
+
+    @Autowired
+    private DailyMenuService dailyMenuService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public String getAllDailyMenus(Model model, Principal principal) {
+        String login = principal.getName();
+        CustomerDto customer = customerService.getCustomer(login);
+        model.addAttribute("foodDiary", dailyMenuService.getAllDailyMenus(customer.getId()));
+        return "fooddiary/foodDiary";
+    }
+}
