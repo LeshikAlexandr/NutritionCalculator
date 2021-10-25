d0957065b37891188c15f57c88c30cedac0953fc
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-25 19:03:21 +0300

    Removed unnecessary autowired

--- src/main/java/com/example/nutritioncalculator/controllers/MainPaigeController.java
+++ src/main/java/com/example/nutritioncalculator/controllers/MainPaigeController.java
@@ -1,6 +1,5 @@
 package com.example.nutritioncalculator.controllers;
 
-import com.example.nutritioncalculator.services.interfaces.CustomerService;
 import com.example.nutritioncalculator.services.interfaces.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,9 +11,6 @@ public class MainPaigeController {
 
     @Autowired
     private PostService postService;
-
-    @Autowired
-    private CustomerService customerService;
 
     @GetMapping("/")
     public String profile(Model model) {
