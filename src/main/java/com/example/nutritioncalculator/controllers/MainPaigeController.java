a6c0cf4c5a6bb9b71e95f060fcfff84dc5cee76d
Alexandr <61452755+LeshikAlexandr@users.noreply.github.com>
2021-10-25 19:02:35 +0300

    Removed unnecessary model attributes

--- src/main/java/com/example/nutritioncalculator/controllers/MainPaigeController.java
+++ src/main/java/com/example/nutritioncalculator/controllers/MainPaigeController.java
@@ -1,15 +1,11 @@
 package com.example.nutritioncalculator.controllers;
 
-import com.example.nutritioncalculator.controllers.dto.CommentDto;
-import com.example.nutritioncalculator.services.interfaces.CommentService;
 import com.example.nutritioncalculator.services.interfaces.CustomerService;
 import com.example.nutritioncalculator.services.interfaces.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @Controller
 public class MainPaigeController {
@@ -20,17 +16,9 @@ public class MainPaigeController {
     @Autowired
     private CustomerService customerService;
 
-    @Autowired
-    private CommentService commentService;
-
     @GetMapping("/")
-    public String profile(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("customer", customerService.getCustomer(principal.getName()));
-        }
+    public String profile(Model model) {
         model.addAttribute("posts", postService.getAllCustomerPosts("admin"));
-        model.addAttribute("comment", new CommentDto());
-        model.addAttribute("customers", customerService.getAllCustomer());
         return "posts/portalNews";
     }
 }
